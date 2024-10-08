0e49736582bd8f17b17694eae80792bc0a83aceb
Vitória Anjos <109820733+anjosvitoria@users.noreply.github.com>
2024-10-07 21:29:29 -0300

    Update

--- src/main/java/com/rocketseat/planner/trip/TripController.java
+++ src/main/java/com/rocketseat/planner/trip/TripController.java
@@ -6,6 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.StreamingHttpOutputMessage;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/trips")
 public class TripController {
@@ -16,15 +19,21 @@ public class TripController {
     @Autowired
     private TripRepository repository;
     @PostMapping
-    public ResponseEntity<String> createTrip(@RequestBody TripRequestPayload payload){
+    public ResponseEntity<TripCreateResponse> createTrip(@RequestBody TripRequestPayload payload){
         Trip newTrip = new Trip (payload);
 
         this.repository.save(newTrip);
 
         this.participantService.registerParticipantsToEvent(payload.emails_to_invite(), newTrip.getId());
 
-        return ResponseEntity.ok("sucesso");
+        return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
 
 
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Trip> getTripDetails(@PathVariable UUID id){
+        Optional<Trip> trip = this.repository.findById(id);
+
+        return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
