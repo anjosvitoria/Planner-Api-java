9369e413243edddd7eb406dbcd0a67fd314c40b0
Vitória Anjos <109820733+anjosvitoria@users.noreply.github.com>
2024-10-22 19:15:32 -0300

    endpoint de confirmação do participante

--- src/main/java/com/rocketseat/planner/participant/ParticipantService.java
+++ src/main/java/com/rocketseat/planner/participant/ParticipantService.java
@@ -22,5 +22,13 @@ public class ParticipantService {
 
     }
 
+    public ParticipantCreateResponse registerParticipantToEvent(String email, Trip trip){
+        Participant newParticipant = new Participant(email, trip);
+        this.repository.save(newParticipant);
+
+        return new ParticipantCreateResponse(newParticipant.getId());
+    };
     public void triggerConfirmationEmailParticipants(UUID tripId){};
+
+    public void triggerConfirmationEmailToParticipant(String email){};
 }
