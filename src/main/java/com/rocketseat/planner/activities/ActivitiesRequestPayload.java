494f807accd85f414aba18bf6c76b520166e23df
Vitória Anjos <109820733+anjosvitoria@users.noreply.github.com>
2024-10-23 20:02:06 -0300

    endpoint de cadastro de viagem

---
+++ src/main/java/com/rocketseat/planner/activities/ActivitiesRequestPayload.java
@@ -0,0 +1,4 @@
+package com.rocketseat.planner.activities;
+
+public record ActivitiesRequestPayload(String title, String occurs_at) {
+}
--- src/main/java/com/rocketseat/planner/trip/TripController.java
+++ src/main/java/com/rocketseat/planner/trip/TripController.java
@@ -1,5 +1,8 @@
 package com.rocketseat.planner.trip;
 
+import com.rocketseat.planner.activities.ActivitiesRequestPayload;
+import com.rocketseat.planner.activities.ActivityResponse;
+import com.rocketseat.planner.activities.ActivityService;
 import com.rocketseat.planner.participant.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +22,9 @@ public class TripController {
 
     @Autowired
     private ParticipantService participantService;
+
+    @Autowired
+    private ActivityService activityService;
 
     @Autowired
     private TripRepository repository;
@@ -101,4 +107,22 @@ public class TripController {
 
         return ResponseEntity.ok(participantList);
     }
+
+    @PostMapping ("/{id}/activities")
+    public ResponseEntity<ActivityResponse> registerActivity(@PathVariable UUID id, @RequestBody ActivitiesRequestPayload payload){
+        Optional<Trip> trip = this.repository.findById(id);
+
+        if (trip.isPresent()){
+            Trip rawTrip = trip.get();
+
+            ActivityResponse activityResponse = this.activityService.registerActivity(payload, rawTrip);
+
+
+            return ResponseEntity.ok(activityResponse);
+        }
+
+        return ResponseEntity.notFound().build();
+
+    }
+
 }
